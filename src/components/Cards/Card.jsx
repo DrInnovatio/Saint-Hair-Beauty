64160a7d87e83776190de68540edc2e319454834
DrInnovatio <turing1723@gmail.com>
2020-03-30 22:13:48 +1000

    added [about me] [our service] sections.

---
+++ src/components/Cards/Card.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import perm from '../../images/perm.png';
+
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 200,
+  },
+});
+
+export default function MediaCard() {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={perm}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    
+    </Card>
+  );
+}
--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
+import MediaCard from '../Cards/Card.jsx'
 import './Landing.css';
+import hairDresser from '../../images/hairDresser.jpg';
+
 
 class Landing extends React.Component {
   render(){
     return(
-      <div >
-        <h1>Landing</h1>
+      <div className="main-landing">
+
+          <div className="banner">
+            <h1>Angela Park</h1>
+            <hr />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam corrupti accusamus nobis veniam quidem, similique dignissimos nemo voluptates laborum suscipit, temporibus incidunt repudiandae</p>
+          </div>
+
+          <div className="about-me">
+            <h1>About Me</h1>
+            <div className="about-me-context">
+              <img src={hairDresser} className="she" alt="she"/>
+              <div className="introduction">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et perspiciatis libero mollitia quos. Illo est, alias quod dolorem similique perspiciatis quisquam nihil aperiam repellat distinctio labore nemo illum exercitationem eum?  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et perspiciatis libero mollitia quos. Illo est, alias quod dolorem similique perspiciatis quisquam nihil aperiam repellat distinctio labore nemo illum exercitationem eum?</p>
+              </div>
+            </div>            
+          </div>
+
+          <div className="cards">
+            <h1>Our Services</h1>
+            <div className="tiles">
+              <MediaCard/>
+              <MediaCard/>
+              <MediaCard/>
+            </div>
+          </div>
+
       </div>
     )
   }
